10848feeb40daeda0e25c90842b4bfd9bec46eb3
Amannigam1820 <amannigam18082001@gmail.com>
2025-01-20 13:51:37 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class schema1(BaseModel):
+    Name:str
+    Class:str
+    Roll_No:int
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message":"Hello World"}
+
+
+# path parameter
+
+@app.get("/item/{item}")
+def pathparams(item):
+    var_name = {"heeelo": item}
+    return {"Message":var_name}
+
+
+# Query parameter
+
+@app.get("/query")
+def queryParams(name:str, roll_no:int):
+    var_name = {"name" : name, "roll_no":roll_no}
+    return (var_name)
+
+    
+@app.post("/items")
+async def create_item(item:schema1):
+    return item
+
+ 
+
+# form data
+
+@app.post("/form-data")
+async def formData(username:str):
+    return {"username":username}
